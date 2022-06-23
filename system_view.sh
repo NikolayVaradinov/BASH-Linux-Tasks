005fe95ab69a23b1b030b3bd988b44c7814b6e20
NikolayVaradinov <70464482+NikolayVaradinov@users.noreply.github.com>
2022-06-23 13:14:15 +0300

    system_view improve (#4)

--- system_view.sh
+++ system_view.sh
@@ -1,19 +1,26 @@
 #! /bin/bash
 
-cpu_cores=`dmidecode -t 4 | grep 'Core Count' | awk '{print $3}'`
-cc () { echo "CPU CORES COUNT: $cpu_cores"; }
+cc () { echo -n "CPU CORES COUNT: "
+	dmidecode -t 4 | grep 'Core Count' | awk '{print $3}'
+}
 
-disk_size=`df -H | grep 'dev' | awk 'NR==1 {print $2}'`
-ds () { echo "DISK SIZE: $disk_size"; }
+ds () { echo -n "DISK SIZE: "
+	df -H | grep 'dev' | awk 'NR==1 {print $2}'
+}
 
-memory=`free -g | awk 'NR==2 {print $2}'`
-ram () { echo "RAM MEMORY SIZE: "$memory"G"; }
+ram () { echo -n "RAM MEMORY SIZE: "
+	free -g | awk 'NR==2 {print $2}''G'
+}
 
-last_users=`last -10 | grep -v 'reboot' | awk 'NR<6 {print $1, $4, $5, $6, $7, $8, $9, $10}'`
-logins () { echo "LAST LOGINS:";echo;echo "$last_users"; }
+logins () { echo "LAST LOGINS:";echo
+	last -10 | grep -v 'reboot' | awk 'NR<6 {print $1, $4, $5, $6, $7, $8, $9, $10}';echo
+}
 
-active_py_perl_ps=`ps -ef | grep -c 'python\|perl'`
-pp () { echo "Number of active Python and Perl Proccesses: ""$(($active_py_perl_ps-1))"; }
+pp () 
+{ 
+	echo -n "Number of active Python and Perl Proccesses: "
+	ps -ef | grep -c 'python\|perl' | expr "$(cat)" - 1
+}
 
 Help () {
 	echo;echo "'cc' to show CPU CORES COUNT";echo
@@ -31,22 +38,4 @@ while getopts ":h" option; do
    esac
 done
 
-if [ -z "$1" ]
-then
-	echo
-	echo 'CPU CORES COUNT: '$cpu_cores
-	echo
-	echo 'DISK SIZE: '$disk_size
-	echo
-	echo 'RAM MEMORY SIZE: '$memory'G'
-	echo
-	echo 'LAST LOGINS:'
-	echo
-	echo "$last_users" 
-	echo
-	echo 'Number of active Python and Perl Proccesses: '"$(($active_py_perl_ps-1))"
-	echo
-
-else
-	$1
-fi		
+$1		
